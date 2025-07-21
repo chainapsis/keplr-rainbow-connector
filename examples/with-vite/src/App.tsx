ba5ca57b797a2b684837f69666bb56372239e6ac
kws <kws1207@kaist.ac.kr>
2025-07-21 20:01:00 +0900

    Add injected provider detecting text to example

--- examples/with-vite/src/App.tsx
+++ examples/with-vite/src/App.tsx
@@ -1,14 +1,17 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 function App() {
   return (
     <div
       style={{
-        display: 'flex',
-        justifyContent: 'flex-end',
+        display: "flex",
+        justifyContent: "flex-end",
         padding: 12,
+        gap: 12,
+        alignItems: "center",
       }}
     >
+      {Object.hasOwn(window, "keplr") ? "Keplr Injected" : "Keplr Not Injected"}
       <ConnectButton />
     </div>
   );
